d97f8a3ba1e07e6d9eab49d474df3cd0d5842fc8
Sriram Balasubramanian <ramkalari@gmail.com>
2012-05-31 01:19:58 +0530

    change product table name

--- db/migrate/20120201170705_add_supplier_id_to_products.rb
+++ db/migrate/20120201170705_add_supplier_id_to_products.rb
@@ -1,5 +1,5 @@
 class AddSupplierIdToProducts < ActiveRecord::Migration
   def change
-    add_column :products, :supplier_id, :integer
+    add_column :spree_products, :supplier_id, :integer
   end
 end
